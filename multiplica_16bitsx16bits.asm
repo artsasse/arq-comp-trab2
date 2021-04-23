97afa6f9414c61e8f6e35da094f24d5af5fba9ca
Alexandre Camillo <alexandrecg@dcc.ufrj.br>
2021-04-23 17:23:11 -0300

    add file with multiplication routine between 16 bits and 16 bits

--- multiplica_16bitsx16bits.asm
+++ multiplica_16bitsx16bits.asm
@@ -4,34 +4,25 @@
 ; Data: 22/04/2021
 ;---------------------------------------------------
 ORG 100h
-nums: DS 5
-nums_qtd: DB 0
-count: DB 5
-
-decimal: DS 2
-
-aux: DS 2
 
 multresult: DS 2
-multiplicando: DS 1
+multiplicando: DS 2
 multiplicador: DS 2
 multcounter: DS 2
 
-ult_end: DS 2
-
-
-char: DS 1
 ORG 0
 
 LDS #200h
 
 main:
-        LDA #3
-        STA multiplicando
-
         LDA #0
-        STA multiplicador
+        STA multiplicando
         LDA #1
+        STA multiplicando + 1
+
+        LDA #3
+        STA multiplicador
+        LDA #0
         STA multiplicador+1
 
         LDA #0
@@ -40,23 +31,25 @@ main:
 
         JSR multiplica
 
-        LDS #31h
+        LDA #31h
 
         HLT
 
 multiplica:
-        LDA multresult     ; Pega o byte menos significativo
-        ADD multiplicando  ; Soma o byte menos significativo com o multiplicando
-        STA multresult     ; Guarda o resultado
 
-        LDA #0             ; Zera o acumulador
 
-        ADC multresult+1   ; Pega o byte mais significativo
-        STA multresult+1   ; e soma o carry (1 caso a soma anterior dê overflow)
+        LDA multresult      ; Pega o byte menos significativo
+        ADD multiplicando   ; Soma o byte menos significativo com o multiplicando
+        STA multresult      ; Guarda o resultado
 
-        LDA multiplicador  ;
-        SUB #1             ;
-        STA multiplicador  ;
+        LDA multiplicando+1 ; Zera o acumulador
+
+        ADC multresult+1    ; Pega o byte mais significativo
+        STA multresult+1    ; e soma o carry (1 caso a soma anterior dê overflow)
+
+        LDA multiplicador   ;
+        SUB #1              ;
+        STA multiplicador   ;
 
         LDA multiplicador+1   ; Pega o byte mais significativo do multiplicador
 
